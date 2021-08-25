ebea97e233085644b32886f4d458fde8f13b4028
AdmiralXy <gameproblem80@gmail.com>
2021-08-25 22:20:06 +0300

    make norms

--- ps_utils.c
+++ ps_utils.c
@@ -6,28 +6,33 @@
 /*   By:  <kricky@student.21-school.ru>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/19 23:27:39 by                   #+#    #+#             */
-/*   Updated: 2021/08/25 22:06:19 by                  ###   ########.fr       */
+/*   Updated: 2021/08/25 22:19:34 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_print_stack(const t_collection *stacks, const char array)
+t_stack	*ft_print_helper(const t_collection *stacks, const char array)
 {
-	t_stack	*head;
-
 	if (array == 'a')
 	{
 		ft_putstr_fd("---------------------------   "
 			"A Stack   ---------------------------\n", 1);
-		head = stacks->a;
+		return (stacks->a);
 	}
 	else
 	{
 		ft_putstr_fd("---------------------------   "
 			"B Stack   ---------------------------\n", 1);
-		head = stacks->b;
+		return (stacks->b);
 	}
+}
+
+void	ft_print_stack(const t_collection *stacks, const char array)
+{
+	t_stack	*head;
+
+	head = ft_print_helper(stacks, array);
 	while (head)
 	{
 		ft_putnbr_fd(head->value, 1);
